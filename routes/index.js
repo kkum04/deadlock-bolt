076eed79da92ccf94354658373e85b0dbe0df878
KenPark <kkum04@gmail.com>
2021-01-18 20:54:57 +0900

    deadlock 출입문 로직 변경 적용

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ const router = express.Router();
 require('dotenv').config();
 
 const LOCK_CONTROL_PIN_CODE = process.env.LOCK_CONTROL_PIN_CODE;
-const DOOR_STATUS_PIN_CODE = process.env.DOOR_STATUS_PIN_CODE;
-const DOOR_SWITCH_PIN_CODE = process.env.DOOR_SWITCH_PIN_CODE;
-const AUTO_LOCK_COUNT = process.env.AUTO_LOCK_COUNT;
 const DOOR_TYPE = process.env.DOOR_TYPE
 
 /* GET home page. */
@@ -64,104 +61,16 @@ const closeDoor = () => {
   return operateDoor(true)
 }
 
-
 const operateDoor = async (isLock) => {
+  if (DOOR_TYPE === 'DEADLOCK_BOLT') {
+    isLock = !isLock
+  }
   const operation = isLock === true ? 'high' : 'low';
   const commandLine = `gpio export ${LOCK_CONTROL_PIN_CODE} ${operation}`;
   return execWithPromise(commandLine);
 }
 
-const initDoorStatusPin = async () => {
-  return execWithPromise(`gpio -g mode ${DOOR_STATUS_PIN_CODE} up`);
-};
-
-const readPinData = async () => {
-  return execWithPromise(`gpio exports`);
-}
-
-const getPinData = (readLines, pinCode) => {
-  const pinData = readLines.split('\n')
-    .map(it => it.trim())
-    .map(it => it.replace(':', ''))
-    .map(it => {
-      return it.split(/(\s+)/)
-        .map(it => it.trim())
-        .filter(it => it.length > 0)
-    })
-    .find(it => pinCode == it[0])
-    [2];
-  return parseInt(pinData);
-}
-
-const checkDoor = async () => {
-  try {
-    await initDoorStatusPin();
-  } catch (e) {
-    throw e;
-  }
-
-  let autoLockCount = 0;
-  setInterval(async () => {
-    try {
-      let readLines = await readPinData();
-      const isLock = getPinData(readLines, LOCK_CONTROL_PIN_CODE) === 1;
-      const isOpenDoor = getPinData(readLines, DOOR_STATUS_PIN_CODE) === 1;
-
-      //////////////////////////////////////////////////////
-      // 문이 닫혀 있고, 락이 안걸려 있다면 5초 후에 문을 자동으로 닫는다.
-      if (!isOpenDoor && !isLock) {
-        autoLockCount++;
-        console.log(`auto lock count: ${autoLockCount}`);
-      } else {
-        autoLockCount = 0;
-      }
-      if (autoLockCount >= AUTO_LOCK_COUNT) {
-        await operateDoor(true);
-        autoLockCount = 0;
-        console.log('auto lock');
-      }
-      //////////////////////////////////////////////////////
-    } catch (e) {
-      console.error(e);
-      console.error('Can not read pin data.');
-    }
-
-  }, 1000);
-}
-
-execWithPromise(`gpio export ${DOOR_STATUS_PIN_CODE} IN`)
-  .then(() => execWithPromise(`gpio export ${LOCK_CONTROL_PIN_CODE} OUT`))
+execWithPromise(`gpio export ${LOCK_CONTROL_PIN_CODE} OUT`)
   .catch(error => console.error(error))
-  .then(() => {
-    if (DOOR_TYPE === "DEADLOCK_BOLT") {
-      checkDoor()
-        .catch(error => {
-          console.error('An error is occurred in checkDoor().');
-          console.error(e);
-        });
-
-      execWithPromise(`gpio export ${DOOR_SWITCH_PIN_CODE} IN`)
-        .then(() => execWithPromise(`gpio -g mode ${DOOR_SWITCH_PIN_CODE} down`))
-        .then(() => checkSwitch())
-    }
-  })
-
-const checkSwitch = () => {
-  setInterval(async () => {
-    try {
-      let readLines = await readPinData();
-      const isOn = getPinData(readLines, DOOR_SWITCH_PIN_CODE) === 1;
-
-      if (isOn) {
-        console.log(`switch is pushed`);
-        await openDoor()
-        console.log('Open')
-      }
-    } catch (e) {
-      console.error('checkSwitch error')
-      console.error(e)
-    }
-  }, 100)
-}
 
 module.exports = router;
